6398eefe6daec714dc6c3b3ac883af8eabcdc6c4
NIMADREZ <soyyo982010@hotmail.com>
2021-11-13 18:17:31 -0600

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Prrootse";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 651;
 
 // Crea una variable booleana:
 const nuevoBool = null;
